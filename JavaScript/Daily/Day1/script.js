4ee403b20f648d12e1fc31f227433b83ca88fa57
Sharmaji9211 <shivamsharma03052002@gmail.com>
2025-03-21 20:01:55 +0530

    Assignment 4 Completeted

---
+++ JavaScript/Daily/Day1/script.js
@@ -0,0 +1,30 @@
+console.log("Hello World");
+// var a=40;
+// alert("a = "+a);
+
+num= 40;
+console.log(num);
+var num;  
+
+
+
+let num1=55;
+console.log(num1);
+
+ let num2 = 45;
+ let num3="35";
+ console.log(num2+num3);
+ console.log(num2-num3);
+ console.log(num2*num3);
+ console.log(num2/num3);
+ console.log(num<=num3);
+ console.log();
+
+ let b= 43;
+ console.log(b,b++,--b,b--,b,++b,b,b++,++b,--b,--b,b++);
+
+
+ 
+ 
+ 
+ 
